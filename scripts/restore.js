fc4b65a5155f4a75be8c0a3cb4da8cf40c188fd3
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-06-10 12:22:35 +0800

    fix homepage setup for npm publish

--- scripts/restore.js
+++ scripts/restore.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 let fs = require('fs')
 let data = require('../package.json')
-data.homepage = 'https://github.com/beenotung/react-any-data#readme'
+data.homepage = 'https://github.com/beenotung/react-use-ionic-form#readme'
 let text = JSON.stringify(data, null, 2) + '\n'
 fs.writeFileSync('package.json', text)
 
